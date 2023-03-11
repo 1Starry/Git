9a430183c805fc59de1f4ce587c11b6f9c24991c
Starry <3509879562@qq.com>
2023-03-11 19:44:24 +0800

    master commit

--- src/main/java/org/example/git/Git.java
+++ src/main/java/org/example/git/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("hello");
         System.out.println("hello123");
         System.out.println("hello1234");
+        System.out.println("hello1234master");
     }
 }
