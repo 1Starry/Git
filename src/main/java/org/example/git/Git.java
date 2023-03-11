b2bb4754049f29adf235ad8b9e0fbc454830f7fd
Starry <3509879562@qq.com>
2023-03-11 19:42:06 +0800

    hot-fix commit

--- src/main/java/org/example/git/Git.java
+++ src/main/java/org/example/git/Git.java
@@ -8,5 +8,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello123");
+        System.out.println("hello1234");
     }
 }
